c10ee129f297e33da546be7ce15e86bca511fb00
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-01-15 11:07:54 -0800

    Sample project for pathing jar

---
+++ src/main/java/netflix/Application.java
@@ -0,0 +1,10 @@
+package netflix;
+
+
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("TEST");
+        Thread.sleep(60  * 1000L * 1000L);
+    }
+}
